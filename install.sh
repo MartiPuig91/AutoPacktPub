efc409f9b0e40d9904e3d1a749ad53ffc19161d2
Martí Puig <mpuig1991@gmail.com>
2017-02-13 15:57:09 +0100

    Script install dependencies

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 13th February 2017
+# created by: MartiPuig91
+
+#DESCRIPTION: Install dependencies for execute claimBook.sh
+
+
+# IMPORTANT: 
+#		This script isn't malicious but I (the developer) am not responsible of ANY damage in your computer, files and any
+#		problem with your www.packtpub.com account.
+# 		USE IT BY YOUR OWN RISK
+
+cd 
+sudo apt-get install sendemail -y
+sudo apt-get install libnet-ssleay-perl -y
+sudo apt-get install libio-socket-ssl-perl
